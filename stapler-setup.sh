b4b7d7273867f36b755e8cc68110a95d2a9898c1
Tyler Stapler <tstapler@iastate.edu>
2015-12-01 15:16:35 -0600

    Fixed the linking for the configs

--- stapler-setup.sh
+++ stapler-setup.sh
@@ -5,19 +5,23 @@ echo "Installing Tyler's Config"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR 
 
-#Install spf-13 vim
-if [ -z "${VIMRUNTIME+x}" ]
-then 
-    sh <(curl https://j.mp/spf13-vim3 -L)
-else
-    echo "You need vim installed to install spf-13 vim"
+if [ ! -d ~/.spf13-vim-3/ ];
+then
+    #Install spf-13 vim
+    if [ -z "${VIMRUNTIME+x}" ];
+    then 
+        sh <(curl https://j.mp/spf13-vim3 -L)
+    else
+        echo "You need vim installed to install spf-13 vim"
+    fi
+fi
 
 
 #Switch to the install directory
 cd $DIR
 
 #Create links to the files in the user's home directory
-shopt -s extglob
-ln -s ./!(setup.sh|.git) ~/
-shopt -u extglob
+echo "Linking the following files to the home directory"
+bash -O extglob -c 'echo !(*-setup.sh*|.git|.|..)'
+bash -O extglob -c 'ln -s !(*-setup.sh*|.git|.|..) ~/'
 
