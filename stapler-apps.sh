52e83bcd341e3d6f0f9bf9e2d5c8c82b5bd3cde6
Tyler Stapler <tstapler@iastate.edu>
2015-12-02 18:05:45 -0600

    Excluded app install script from copying and Improved vim config

--- stapler-apps.sh
+++ stapler-apps.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
+#Store the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR 
 
-echo "Installing spf-13 and vim related files"
 #Files for Vim Plugins
-sudo apt-get install ack-grep silversearcher-ag
+sudo apt-get install ack-grep silversearcher-ag dnf automake gcc gcc-c++ kernel-devel cmake python-devel
 
 #Install spf-13 vim
 if [ ! -d ~/.spf13-vim-3/ ];
+echo "Installing spf-13 and vim related files"
 then
     if [ -z "${VIMRUNTIME+x}" ];
     then 
         sh <(curl https://j.mp/spf13-vim3 -L)
+        echo "Compiling YouCompleteMe"
+        cd ~/.vim/bundle/YouCompleteMe
+        ./install.py --clang-completer
     else
         echo "You need vim installed to install spf-13 vim"
     fi
 fi
 
+echo "Installing tmux/wemux \n"
+
+if [ ! -d /usr/local/share/wemux ]; then
+   sudo git clone git://github.com/zolrath/wemux.git /usr/local/share/wemux
+   sudo ln -s /usr/local/share/wemux/wemux /usr/local/bin/wemux
+   sudo cp -rs $DIR/wemux.conf /usr/local/etc/wemux.conf
+
+   echo "Installing wemux-pair"
+   gem install wemux-pair
+fi
 
--- stapler-setup.sh
+++ stapler-setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-echo "Installing Tyler's Config"
+
+echo "Installing Tyler's Config \n"
 
 #Store the directory of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR 
 
 #Switch to the install directory
+
 cd $DIR
 
 #Create links to the files in the user's home directory
 echo "Linking the following files to the home directory"
 bash -O extglob -c 'echo '"$DIR"'/!(*-setup.sh*|.git|.|..)'
-bash -O extglob -c 'ln -s '"$DIR"'/!(*-setup.sh*|.git|.|..|.config) ~/'
+bash -O extglob -c 'ln -s '"$DIR"'/!(*stapler*|.git|.|..|.config) ~/'
 cp -rs $DIR/.config/ ~/
