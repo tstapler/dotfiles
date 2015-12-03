b02e4ebf9473c77f5da25b4bb365b5993f40c6b2
Tyler Stapler <tstapler@iastate.edu>
2015-12-03 10:38:04 -0600

    Added autoformat and eclim plugin to vim. Formatted app installer

--- stapler-apps.sh
+++ stapler-apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Set Fish as the default script for future runs if available
-if [ -a /usr/bin/fish ]; then
+if [ -a /usr/bin/fish ] && [ "$SHELL" != "/usr/bin/fish" ]; then
     echo "Setting Fish as Default Shell"
     chsh -s 'which fish'
 fi
@@ -10,32 +10,55 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR 
 
-#Files for Vim Plugins
-sudo apt-get install ack-grep silversearcher-ag dnf automake gcc gcc-c++ kernel-devel cmake python-devel
+#Install Applications
+sudo apt-get install eclipse vim-gtk
+
+
+#Install Development Tools
+sudo apt-get install nodejs
+
+#Install Node Packages
+npm install -g js-beautify
+
+#Install Ruby Gems
+gem install ruby-beautify
+
+#Install Libraries and Build Utilities
+sudo apt-get install dnf automake gcc gcc-c++ kernel-devel cmake python-devel openjdk-7-jdk
 
 #Install spf-13 vim
-if [ ! -d ~/.spf13-vim-3/ ];
-echo "Installing spf-13 and vim related files"
-then
+if [ ! -d ~/.spf13-vim-3/ ]; then
+    echo "Installing vim, spf-13, and related files"
     if [ -z "${VIMRUNTIME+x}" ];
     then 
         sh <(curl https://j.mp/spf13-vim3 -L)
+
         echo "Compiling YouCompleteMe"
         cd ~/.vim/bundle/YouCompleteMe
         ./install.py --clang-completer
+
+        #TODO:Install Eclim Support
+        #TODO:Install Eclipse 4.5 (Mars)
+
+
     else
         echo "You need vim installed to install spf-13 vim"
     fi
+    sudo apt-get astyle clang-format ack-grep silversearcher-ag python-autopep8 tidy
 fi
 
 echo "Installing tmux/wemux \n"
 
 if [ ! -d /usr/local/share/wemux ]; then
-   sudo git clone git://github.com/zolrath/wemux.git /usr/local/share/wemux
-   sudo ln -s /usr/local/share/wemux/wemux /usr/local/bin/wemux
-   sudo cp -rs $DIR/wemux.conf /usr/local/etc/wemux.conf
+    sudo git clone git://github.com/zolrath/wemux.git /usr/local/share/wemux
+    sudo ln -s /usr/local/share/wemux/wemux /usr/local/bin/wemux
+    sudo cp -rs $DIR/wemux.conf /usr/local/etc/wemux.conf
 
-   echo "Installing wemux-pair"
-   gem install wemux-pair
+    echo "Installing wemux-pair"
+    gem install wemux-pair
 fi
 
+#Install pyenv
+if [ ! -d ~/.pyenv ]; then
+    curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+fi
