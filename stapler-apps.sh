c985ae6b9822424a9d7ca656a0b3f8df57a8d4d2
Tyler Stapler <tstapler@iastate.edu>
2016-01-14 14:09:04 -0600

    Renames perl packages, moves script, removes eclipse install

--- stapler-apps.sh
+++ stapler-apps.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR 
 
 #Install Applications
-sudo apt-get install eclipse vim-gtk
+sudo apt-get install vim-gtk
 
 
 #Install Development Tools
