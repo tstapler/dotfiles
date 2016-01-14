67b67682e24b98a2f2b83fc79aed00bb545e1fb9
Tyler Stapler <tstapler@iastate.edu>
2016-01-14 03:08:35 -0600

    Adds the beginnings of the python installer
    The installer will now install any of the apt-get packages defined in
    the apps.yml file. Using similar methods I will be able to install the
    rest of the programs in other yml files.

--- stapler-installer.py
+++ stapler-installer.py
@@ -1,31 +1,47 @@
-import sys, click
+import glob, os, sys, click, subprocess
+from yaml import load, dump
+from plumbum import local, FG
+from plumbum.cmd import sudo
 
-class Installer:
-    def __init__(self):
-        self.install_platform = sys.platform
+install_platform = sys.platform
+config_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(config_dir)
 
-    def install_apps():
+@click.group()
+@click.option('--install_all', is_flag=True)
+@click.option('--uninstall', is_flag=True)
+def cli(install_all, uninstall):
+    """Tyler Stapler's Dev environment installer. Currently for *nix and soon for Windows"""
+
+    if install_all :
+        pass
+    elif uninstall:
+        pass
+    else:
         pass
 
-    def install_libs():
-        pass
+@cli.command()
+def apps():
+    apt_get = sudo[local["apt-get"]]
+    apps = load(open(os.path.join(config_dir, "apps.yml"), "r"))
+    for app in apps["standalone"]:
+        print(apt_get("install", app))
+        
+@cli.command()
+def node_packages():
+    npm = local["npm"]
+    pass
 
-    def configure_env():
-        """Make changes to existing system configuration"""
-        pass
+@cli.command()
+def perl_packages():
+    pass
 
-    def link_config_files():
-        pass
+@cli.command()
+def ruby_gems():
+    pass
 
 
-
-@click.command()
-@click.option('--all', 'install_type', is_flag=True)
-@click.option('--apps', 'install_type', is_flag=True)
-@click.option('--libs', 'install_type', is_flag=True)
-@click.option('--uninstall', 'install_type', is_flag=True)
-def cli(all, apps, libs, uninstall):
-    """Tyler Stapler's Dev environment installer. Currently for *nix and Windows"""
+def libs():
     pass
 
 if __name__ == '__main__':
