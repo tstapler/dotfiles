ad13bc6711a87a0b6b20147970f349923bc2c36c
Tyler Stapler <tstapler@iastate.edu>
2016-01-13 23:53:36 -0600

    Moves the install scripts into logical partitions

--- stapler-setup.sh
+++ stapler-setup.sh
@@ -12,6 +12,9 @@ cd $DIR
 
 #Create links to the files in the user's home directory
 echo "Linking the following files to the home directory"
-bash -O extglob -c 'echo '"$DIR"'/!(*stapler*|.git|.|..|.config)'
-bash -O extglob -c 'ln -s '"$DIR"'/!(*stapler*|.git|.|..|.config) ~/'
+shopt -s extglob dotglob
+echo $DIR/!(*stapler*|.git|.|..|.config) 
+echo ""
+cp --symbolic-link $DIR/!(*stapler*|.git|.|..|.config) ~/
 cp -rs $DIR/.config/ ~/
+shopt -u extglob dotglob
