7b57d006911f1e9a1fc017d72a620e4a60cd520f
Tyler Stapler <tstapler@iastate.edu>
2016-01-15 14:52:43 -0600

    Separated out last script and deleted unneeded file

---
+++ fishshell-default.sh
@@ -0,0 +1,5 @@
+#Set Fish as the default script for future runs if available
+if [ -a /usr/bin/fish ] && [ "$SHELL" != "/usr/bin/fish" ]; then
+    echo "Setting Fish as Default Shell"
+    chsh -s /usr/bin/fish $USER
+fi
