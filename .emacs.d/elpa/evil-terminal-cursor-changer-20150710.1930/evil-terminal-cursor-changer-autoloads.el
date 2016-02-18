c4d28010119e55b45393542f9981aa7a098ed725
Tyler Stapler <tstapler@iastate.edu>
2016-02-17 18:35:00 -0600

    Consolidated config to the one true editor

--- .emacs.d/elpa/evil-terminal-cursor-changer-20150710.1930/evil-terminal-cursor-changer-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; evil-terminal-cursor-changer-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("evil-terminal-cursor-changer-pkg.el"
-;;;;;;  "evil-terminal-cursor-changer.el") (21952 55944 796971 624000))
-
-;;;***
-
-(provide 'evil-terminal-cursor-changer-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; evil-terminal-cursor-changer-autoloads.el ends here
--- .emacs.d/elpa/evil-terminal-cursor-changer-20150710.1930/evil-terminal-cursor-changer-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-terminal-cursor-changer" "20150710.1930" "Change cursor shape by evil state on terminal." (quote ((evil "1.0.8"))))
