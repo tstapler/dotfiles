0ab2bbeb4f8f973606a390574e4f74dd75a0d87d
Tyler Stapler <tystapler@gmail.com>
2016-04-05 13:45:00 -0500

    Adds installer for youtube-dl, git-annex for dropbox

---
+++ stapler-scripts/install-scripts/dropboxannex-install.sh
@@ -0,0 +1,4 @@
+git clone git://github.com/TobiasTheViking/dropboxannex.git ~/.dropbox-annex
+
+chmod +x /home/$USER/.dropbox-annex/git-annex-remote-dropbox
+sudo ln -sf /home/$USER/.dropbox-annex/git-annex-remote-dropbox /usr/local/bin/git-annex-remote-dropbox
