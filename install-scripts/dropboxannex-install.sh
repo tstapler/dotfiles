d30e2be8e1e3bbce1ef1217577698d485990e7f4
Tyler Stapler <tstapler@iastate.edu>
2016-04-05 14:11:05 -0500

    Updated installer to fix python and add several packages

--- install-scripts/dropboxannex-install.sh
+++
@@ -1,4 +0,0 @@
-git clone git://github.com/TobiasTheViking/dropboxannex.git ~/.dropbox-annex
-
-chmod +x /home/$USER/.dropbox-annex/git-annex-remote-dropbox
-sudo ln -sf /home/$USER/.dropbox-annex/git-annex-remote-dropbox /usr/local/bin/git-annex-remote-dropbox
