c058cfae70851efa4e38de7a9000f305e80c3ab5
Tyler Stapler <tystapler@gmail.com>
2016-05-01 16:01:52 -0500

    Adds script for installing dein

--- stapler-scripts/install-scripts/dein-install.sh
+++ stapler-scripts/install-scripts/dein-install.sh
@@ -1,5 +1,6 @@
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-
 sh ./installer.sh ~/.vim/bundle
-
 rm installer.sh
+cd ~/.config/nvim
+ln -s ~/.vimrc init.vim
+
