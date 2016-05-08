cbc9cc791b89585e51372f4a26828f25af0958c1
Tyler Stapler <tystapler@gmail.com>
2016-05-07 20:48:32 -0500

    Generalizes bash compatible shell config

---
+++ .shell/exports.sh
@@ -0,0 +1,41 @@
+# Fixes git + gpg error inside of tmux
+export GPG_TTY=$(tty)
+
+# User path configuration
+export PATH=$PATH:"/home/tstapler/stapler-config/env/bin:/home/tstapler/.pyenv/shims:/home/tstapler/.pyenv/shims:/home/tstapler/.pyenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+
+#Add Dart pub files to PATH
+export PATH=$PATH:"/home/tstapler/.pub-cache/bin"
+
+#Create Go Path
+export GOPATH="~/.local/lib/go"
+
+#Add LaTex files to PATH
+export TEXMFHOME=~/texmf
+
+#Add GO to PATH
+export PATH="$PATH":"$GOPATH/bin"
+
+#Add Cabal to PATH
+export PATH="$HOME/.cabal/bin:"$PATH
+
+#Set environment varibles
+export ENHANCD_FILTER=fzf
+
+#Setup RVM
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+# Editor Variable
+if hash nvim 2>/dev/null; then
+	# Use Neovim if it exists
+	export EDITOR='nvim'
+
+elif hash vim 2>/dev/null; then
+	# Use Vim if no Neovim
+	export EDITOR='vim'
+
+else
+	# Settle for vi if all else fails
+	export EDITOR='vi'
+	alias vim='vi'
+fi
