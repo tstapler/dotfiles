8398ea439a87d375fee3145f789f4b58ebfab78c
Tyler Stapler <tystapler@gmail.com>
2016-05-16 18:10:56 -0500

    Adds check for neovim directory

--- stapler-scripts/install-scripts/dein-install.sh
+++ stapler-scripts/install-scripts/dein-install.sh
@@ -1,6 +1,9 @@
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/bundle
 rm installer.sh
+if [! -d "~/.config" ]; then
+	mkdir ~/.config/nvim
+fi
 cd ~/.config/nvim
 ln -s ~/.vimrc init.vim
 
