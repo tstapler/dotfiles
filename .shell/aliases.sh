680d235dc902bbad0159de3c78c9e72541edfdfb
Tyler Stapler <tystapler@gmail.com>
2016-05-18 12:46:08 -0500

    Adds password generator and updates for osx + new zplug

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -8,6 +8,14 @@ alias ll='ls -lF --color=auto'
 alias la='ls -alF --color=auto'
 alias ls='ls -F'
 
+gen_pass () {
+xkcdpass -c 5 -d "-" -n 3 |
+awk -v NUM=$(echo $RANDOM % 10 | bc) '{printf("%s-%d\n",$1,NUM)}'
+}
+
+
+alias xkp='gen_pass'
+
 # Encourage the use of NeoVim when possible
 if [ "$EDITOR" = "nvim" ]; then
 	alias vim='nvim'
