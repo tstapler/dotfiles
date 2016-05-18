e58df97edcda615f156e643829e5a8187a6d3db4
Tyler Stapler <tystapler@gmail.com>
2016-05-18 12:37:03 -0500

    Updates scripts to work with osx

--- stapler-scripts/install-scripts/dein-install.sh
+++ stapler-scripts/install-scripts/dein-install.sh
@@ -1,7 +1,7 @@
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.vim/bundle
 rm installer.sh
-if [! -d "~/.config" ]; then
+if [ ! -d "~/.config" ]; then
 	mkdir ~/.config/nvim
 fi
 cd ~/.config/nvim
