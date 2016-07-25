4aee33c63c3806eed77c5f61856d177f697a6776
Tyler Stapler <tstapler@iastate.edu>
2016-07-25 18:54:08 -0500

    Add personal binary folder to path and clean up comments

--- .shell/exports.sh
+++ .shell/exports.sh
@@ -1,25 +1,28 @@
 # Fixes git + gpg error inside of tmux
 export GPG_TTY=$(tty)
 
-#Add Dart pub files to PATH
+# Add Dart pub files to PATH
 export PATH="$PATH":"$HOME/.pub-cache/bin"
 
-#Create Go Path
+# Add personal executables to path
+export PATH=$PATH:"$HOME/bin/bin"
+
+# Create Go Path
 export GOPATH="$HOME/.local/lib/go"
 
-#Add LaTex files to PATH
+# Add LaTex files to PATH
 export TEXMFHOME=~/texmf
 
-#Add GO executables to PATH
+# Add GO executables to PATH
 export PATH="$PATH":"$GOPATH/bin"
 
-#Add Cabal to PATH
+# Add Cabal to PATH
 export PATH="$HOME/.cabal/bin:"$PATH
 
-#Set environment varibles for Enhancd
+# Set environment varibles for Enhancd
 export ENHANCD_FILTER=fzf
 
-#Setup RVM
+# Setup RVM
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 
 # Editor Variable
