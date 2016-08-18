b18dce0f180fde868684996702675d5920be3ed2
Tyler Stapler <tystapler@gmail.com>
2016-08-18 09:21:03 -0500

    Add function for switching ssh keys easier

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -8,12 +8,16 @@ alias ll='ls -lF --color=auto'
 alias la='ls -alF --color=auto'
 alias ls='ls -F'
 
-gen_pass () {
+function gen_pass  {
 xkcdpass -c 5 -d "-" -n 3 |
 awk -v NUM=$(python -c "import random; print random.randrange(0,10)") '{printf("%s-%d\n",$1,NUM)}'  |
 python -c 'import sys; sys.stdout.write(sys.stdin.read().title())'
 }
 
+function ssh-switch {
+	ssh-add -D
+	ssh-add ~/.ssh/$1
+}
 
 alias xkp='gen_pass'
 
