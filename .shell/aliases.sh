7a41c408b3bf97314166604582f6b437b32b830a
Tyler Stapler <tystapler@gmail.com>
2016-08-22 14:18:39 -0500

    Add hub override alias

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -8,6 +8,10 @@ alias ll='ls -lF --color=auto'
 alias la='ls -alF --color=auto'
 alias ls='ls -F'
 
+if hash hub 2>/dev/null; then
+	alias git=hub
+fi
+
 function gen_pass  {
 xkcdpass -c 5 -d "-" -n 3 |
 awk -v NUM=$(python -c "import random; print random.randrange(0,10)") '{printf("%s-%d\n",$1,NUM)}'  |
