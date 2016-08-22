f3a145e65aca8b672c4737a2789c69b8db2a65dd
Tyler Stapler <tystapler@gmail.com>
2016-08-22 14:19:04 -0500

    Configure virtualenvwrapper if present

--- .shell/languages.sh
+++ .shell/languages.sh
@@ -8,3 +8,13 @@ fi
 if [[ -d "~/.dnx" ]]; then
 [ -s "/home/tstapler/.dnx/dnvm/dnvm.sh" ] && . "/home/tstapler/.dnx/dnvm/dnvm.sh" # Load dnvm
 fi
+
+if [[ -f "/usr/local/bin/virtualenvwrapper.sh" ]]; then
+	if [[ $WORKIVA == true ]]; then
+		export PROJECT_HOME=$HOME/Workiva
+	else
+		export PROJECT_HOME=$HOME/Programming/Python
+	fi
+	export WORKON_HOME=$HOME/.virtualenvs
+	source /usr/local/bin/virtualenvwrapper.sh
+fi
