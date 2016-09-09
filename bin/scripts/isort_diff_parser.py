2af4b33478fe3f56a08b6feaa0ced1d5bca35c3e
Tyler Stapler <tystapler@gmail.com>
2016-09-09 08:58:13 -0500

    Update isort parser and add isort-lint wrapper

--- bin/scripts/isort_diff_parser.py
+++ bin/scripts/isort_diff_parser.py
@@ -7,12 +7,13 @@ ErrorMessage = collections.namedtuple('ErrorMessage', ['file', 'srcline',
                                                        'destline', 'message'])
 
 file_re = re.compile(r'--- (.*\.py)')
-range_re = re.compile(r'@@ -(.*),\d+ \+')
+range_re = re.compile(r'@@ -(.*),\d+ \+(.*),')
 
 
 def main():
     file = ""
-    line_number = 1
+    old_number = 1
+    new_number = 1
 
     messages = []
 
@@ -22,39 +23,55 @@ def main():
 
     for line in fileinput.input():
         if line.startswith(" "):
-            line_number += 1
+            old_number += 1
+            new_number += 1
         elif line.startswith("---"):
             file = file_re.match(line).group(1)
             continue
         elif line.startswith("+++"):
             continue
         elif line.startswith("@@"):
-            line_number = int(range_re.match(line).group(1))
+            old_number = int(range_re.match(line).group(1))
+            new_number = int(range_re.match(line).group(2))
         elif line.startswith("+"):
             content = line[1:].rstrip('\n')
             if content in src:
                 messages.append(ErrorMessage(file, src[content],
-                                             line_number, content))
+                                             new_number, content))
                 del src[content]
+            elif content == "":
+                messages.append(ErrorMessage(file, None,
+                                             new_number, "Add a line"))
             else:
-                dest[content] = line_number
-            line_number += 1
-            continue
+                dest[content] = new_number
+            new_number += 1
         elif line.startswith("-"):
             content = line[1:].rstrip('\n')
             if content in dest:
-                messages.append(ErrorMessage(file, line_number,
+                messages.append(ErrorMessage(file, old_number,
                                              dest[content], content))
                 del dest[content]
+            elif content == "":
+                messages.append(ErrorMessage(file, old_number,
+                                             None, "Remove a line"))
             else:
-                src[content] = line_number
-            line_number += 1
-            continue
+                src[content] = old_number
+            old_number += 1
         else:
             pass
 
     for msg in messages:
-        print("{}:Import '{}' should be moved from line {} to line {}".format(msg.file, msg.message, msg.srcline, msg.destline))
+        if not msg.srcline:
+            print('{file}:{line}:{msg}'
+                  .format(file=msg.file, line=msg.destline, msg=msg.message))
+        elif not msg.destline:
+            print('{file}:{line}:{msg}'
+                  .format(file=msg.file, line=msg.srcline, msg=msg.message))
+        else:
+            print('{}:{}:Import statement "{}" should be moved from the '
+                  'old line {} to the new line {}'
+                  .format(msg.file, msg.srcline, msg.message,
+                          msg.srcline, msg.destline))
 
 if __name__ == '__main__':
     main()
