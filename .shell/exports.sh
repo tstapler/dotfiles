6c8bc3d1ea833a5a17df93585b1186948419cdfd
Tyler Stapler <tyler.stapler@workiva.com>
2016-11-11 15:21:33 -0600

    Add fixes for gpg agent

--- .shell/exports.sh
+++ .shell/exports.sh
@@ -44,3 +44,10 @@ else
 fi
 
 export GIT_EDITOR=$EDITOR
+
+if [ -f "${HOME}/.gpg-agent-info" ]; then
+	. "${HOME}/.gpg-agent-info"
+	export GPG_AGENT_INFO
+	export SSH_AUTH_SOCK
+	export SSH_AGENT_PID
+fi
--- .shell/functions.sh
+++ .shell/functions.sh
@@ -18,3 +18,35 @@ function commited_files {
 		
 	git log --pretty=format: --name-only -n $commits | sort | uniq | tr '\n' ' '; echo ''
 }
+
+function gpg_agent_fix {
+	if ! test -v GPG_AGENT_INFO; then
+		if gpg-agent 2>/dev/null; then
+			if test -e /tmp/.gpg-agent-$USER/env; then
+				. /tmp/.gpg-agent-$USER/env
+			elif test -e ~/.gpg-agent-info; then
+				. ~/.gpg-agent-info
+			else
+				echo 'A gpg agent is running, but we cannot find its socket info because'
+				echo 'the GPG_AGENT_INFO env var is not set and gpg agent info has not been'
+				echo 'written to any expected location. Cannot continue. Please report this'
+				echo 'issue for investigation.'
+				exit 5
+			fi
+		else
+			mkdir /tmp/.gpg-agent-$USER
+			chmod 700 /tmp/.gpg-agent-$USER
+			gpg-agent --daemon --write-env-file /tmp/.gpg-agent-$USER/env
+			. /tmp/.gpg-agent-$USER/env
+		fi
+		# The env file doesn't include an export statement
+		export GPG_AGENT_INFO
+	else
+		if ! gpg-agent 2>/dev/null; then
+			echo 'GPG_AGENT_INFO is set, but cannot connect to the agent.'
+			echo 'Unsure how to proceed, so aborting execution. Please report this'
+			echo 'issue for investigation.'
+			exit 5
+		fi
+	fi
+}
