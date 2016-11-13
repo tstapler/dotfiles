3f65463b1d17fa35c080bfaf9e3d0dd37e4f2f8f
Tyler Stapler <tystapler@gmail.com>
2016-11-13 15:12:12 -0600

    Remove unnecessary noise from status bar

--- .i3/statusbar.py
+++ .i3/statusbar.py
@@ -38,38 +38,6 @@ status.register("battery",
             "FULL": "=",
             },)
 
-        # This would look like this:
-# Discharging 6h:51m
-status.register("battery",
-        format="{status} {remaining:%E%hh:%Mm}",
-        alert=True,
-        alert_percentage=5,
-        status={
-            "DIS":  "Discharging",
-            "CHR":  "Charging",
-            "FULL": "Bat full",
-            },)
-
-        # Displays whether a DHCP client is running
-status.register("runwatch",
-        name="DHCP",
-        path="/var/run/dhclient*.pid",)
-
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
-# Note: the network module requires PyPI package netifaces
-status.register("network",
-        interface="eno1",
-        format_up="{v4cidr}",)
-
-# Note: requires both netifaces and basiciw (for essid and quality)
-status.register("network",
-        interface="wlan0",
-        format_up="{essid} {quality:03.0f}%",)
 
 # Shows disk usage of /
 # Format:
@@ -83,8 +51,6 @@ status.register("disk",
 # Note: requires libpulseaudio from PyPI
 status.register("pulseaudio",
         format="♪{volume}",)
-
-status.register("uptime")
 
 status.register("updates",
         backends = [aptget.AptGet()]
