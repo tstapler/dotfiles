d948e9805a4629fe137f2824b4b9d5b4bbdbeddd
Tyler Stapler <tystapler@gmail.com>
2016-12-31 16:53:41 -0600

    Update .zshrc

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -8,6 +8,12 @@ alias ll='ls -lF --color=auto'
 alias la='ls -alF --color=auto'
 alias ls='ls -F'
 
+# Add zmv aliases
+alias zmv='noglob zmv'
+alias zcp='noglob zmv -C'
+alias zln='noglob zmv -L'
+alias zsy='noglob zmv -Ls'
+
 # Use hub which gives git github integration
 if hash hub 2>/dev/null; then
 	alias git=hub
