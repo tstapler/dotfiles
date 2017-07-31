100433556d028855ba25b2419fcf19b435d96bc7
Tyler Stapler <tystapler@gmail.com>
2017-07-31 13:25:20 -0500

    Update stapler-scripts to add powershell dir to windows

---
+++ stapler-scripts/PowerShell/Scripts/ken_script_1.ps1
@@ -0,0 +1,4 @@
+﻿$a = (Get-Date).ToUniversalTime().Date
+$b = New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0
+Write-Output ([Math]::Floor(([Timespan] $a.Subtract($b)).TotalSeconds))
+
