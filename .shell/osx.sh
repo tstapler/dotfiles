aaa88922bd1991826def3f69b1a5832c68aa687e
Tyler Stapler <tystapler@gmail.com>
2017-08-23 17:10:34 -0500

    Remove unneeded docker-machine config

--- .shell/osx.sh
+++ .shell/osx.sh
@@ -1,6 +1,1 @@
-if hash docker-machine 2>/dev/null; then
-	# Export docker-machine environment variables
-	eval $(docker-machine env default)
-fi
-
 alias hangoutscamerafix="sudo killall VCDAssistant"
