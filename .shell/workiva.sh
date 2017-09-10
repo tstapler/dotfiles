ac9a438d6b9f857ac64762e51250b55aac27cd89
Tyler Stapler <tystapler@gmail.com>
2017-09-09 20:06:12 -0500

    Make dart completions more robust, remove google cloud completions

--- .shell/workiva.sh
+++ .shell/workiva.sh
@@ -3,26 +3,24 @@ export VENV=local
 
 alias ddev='pub run dart_dev'
 
-# Use dart_dev completions
-if [ -n "$ZSH_VERSION" ]; then
-	autoload -U compinit
-	compinit
-	autoload -U bashcompinit
-	bashcompinit
-	eval "$(pub global run dart_dev bash-completion)"
-elif [ -n "$BASH_VERSION" ]; then
-	eval "$(pub global run dart_dev bash-completion)"
+if hash pub 2>/dev/null; then
+  # Use dart_dev completions
+  if [ -n "$ZSH_VERSION" ]; then
+    autoload -U compinit
+    compinit
+    autoload -U bashcompinit
+    bashcompinit
+    eval "$(pub global run dart_dev bash-completion)"
+  elif [ -n "$BASH_VERSION" ]; then
+    eval "$(pub global run dart_dev bash-completion)"
+  fi
 fi
+
 OS="$(uname)"
 case $OS in
 	'Darwin')
 	# Setup Groovy
   export GROOVY_HOME='/usr/local/opt/groovy/libexec'
-  GCLOUD_PREFIX='/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk'
-  if [[ -f  "$GCLOUD_PREFIX/path.zsh.inc" ]]; then
-    source "$GCLOUD_PREFIX/path.zsh.inc"
-    source "$GCLOUD_PREFIX/completion.zsh.inc"
-  fi
   ;;
 esac
 
