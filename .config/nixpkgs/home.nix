27feb0daeca75194704bc42bce93e0d337978d7b
Tyler Stapler <tystapler@gmail.com>
2017-11-21 09:27:32 -0600

    Remove gpg agent from nix home

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -14,12 +14,6 @@
     pkgs.httpie
   ];
 
-  services.gpg-agent = {
-    enable = true;
-    defaultCacheTtl = 1800;
-    enableSshSupport = true;
-  };
-
  programs.home-manager = {
     enable = true;
     path = "https://github.com/rycee/home-manager/archive/master.tar.gz";
