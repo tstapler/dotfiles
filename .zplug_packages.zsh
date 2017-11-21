6a3e9a84ecb730bd62a1ed1e7eb4eabc4608462d
Tyler Stapler <tystapler@gmail.com>
2017-11-20 21:57:40 -0600

    Fix zsh completion breakage
    Removing lib/completion was the offending change. This should be a
    lesson to never merge an ton of unrelated changes at once.

--- .zplug_packages.zsh
+++ .zplug_packages.zsh
@@ -1,3 +1,7 @@
+# Let zplug manage itself
+zplug "zplug/zplug", hook-build:'zplug --self-manage'
+
+zplug "willghatch/zsh-hooks"
 
 # Theme
 export TERM="xterm-256color"
@@ -7,23 +11,37 @@ zplug "bhilburn/powerlevel9k", as:theme
 zplug "zsh-users/zsh-completions"
 zplug "Tarrasch/zsh-autoenv"
 zplug "b4b4r07/enhancd", use:"init.sh"
-# zplug "zsh-users/zaw"
-# zplug "clvv/fasd", as:command
+zplug "zsh-users/zaw"
 
+zplug "Schnouki/git-annex-zsh-completion"
+zplug "greymd/docker-zsh-completion"
+zplug "lib/completion", from:oh-my-zsh
+zplug "plugins/httpie", from:oh-my-zsh
 zplug "aswitalski/oh-my-zsh-sensei-git-plugin"
-
-# Suggestions
-# zplug "tarruda/zsh-autosuggestions"
 zplug "djui/alias-tips"
 
-# zplug "zsh-users/zsh-history-substring-search"
+# The file searchers
 
-# Set the priority when loading
-# e.g., zsh-syntax-highlighting must be loaded
-# after executing compinit command and sourcing other plugins
-# (If the defer tag is given 2 or above, run after compinit command)
-# zplug "zsh-users/zsh-syntax-highlighting", defer:2
+case $(uname) in
+	Darwin) 
+		BIN_ARCH=darwin
+		;;
+	*)
+		BIN_ARCH=linux
+		;;
+esac
 
-# Let zplug manage itself
-zplug "zplug/zplug", hook-build:'zplug --self-manage'
+zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf, use:"*$BIN_ARCH*_amd64*"
+
+zplug "peco/peco", as:command, from:gh-r, rename-to:peco, use:"*$BIN_ARCH*64*"
+zplug "clvv/fasd", as:command 
+zplug "stedolan/jq", \
+    from:gh-r, \
+    as:command, \
+    rename-to:jq
+
+# Suggestions
+zplug "tarruda/zsh-autosuggestions"
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-history-substring-search"
 
