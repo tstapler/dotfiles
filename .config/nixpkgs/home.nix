3a70c629eeadaf15a531bf0fb08f3a8db5f04b1f
Tyler Stapler <tystapler@gmail.com>
2017-12-06 11:15:45 -0600

    Add dev tools to config

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -2,16 +2,50 @@
 
 {
   home.packages = [
+    # pkgs.gitAndTools.git-annex
+    # pkgs.gitAndTools.git-annex-remote-rclone
+    # pkgs.pdfgrep
+    pkgs.ag
+    pkgs.automake
     pkgs.crystal
+    pkgs.ctags
+    pkgs.diffutils
+    pkgs.figlet
     pkgs.fzf
     pkgs.fzy
+    pkgs.gdb
     pkgs.gettext
+    pkgs.git
+    pkgs.gitAndTools.hub
+    pkgs.gnupg1
+    pkgs.gnused
     pkgs.htop
-    pkgs.jq
-    pkgs.lnav
-    pkgs.neovim
-    pkgs.peco
     pkgs.httpie
+    pkgs.jq
+    pkgs.keybase
+    pkgs.keychain
+    pkgs.lnav
+    pkgs.ncat
+    pkgs.neovim
+    pkgs.nmap
+    pkgs.pandoc
+    pkgs.patchutils
+    pkgs.peco
+    pkgs.python27Packages.flake8
+    pkgs.python27Packages.grip
+    pkgs.python27Packages.htmltreediff
+    pkgs.python27Packages.isort
+    pkgs.python27Packages.neovim
+    pkgs.python27Packages.pandocfilters
+    pkgs.python36Packages.neovim
+    pkgs.ranger
+    pkgs.rclone
+    pkgs.ripgrep
+    pkgs.shellcheck
+    pkgs.tmux
+    pkgs.toilet
+    pkgs.tree
+    pkgs.valgrind
   ];
 
  programs.home-manager = {
