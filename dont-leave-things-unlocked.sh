f4abeb80c4728ac166fcece6f2478444174d2a19
Tyler Stapler <tystapler@gmail.com>
2018-02-05 15:01:52 -0600

    Add script for unlocked computers

---
+++ dont-leave-things-unlocked.sh
@@ -0,0 +1,27 @@
+SCRIPT_PATH=/tmp/dont-leave-your-computer-unlocked.sh
+
+cat << EOF > $SCRIPT_PATH
+#! /usr/bin/env bash
+
+#sleep \$((RANDOM % 400 ))
+
+# Get a funny quote to say
+QUOTE=\$(curl "http://quotes.rest/qod.json?category=funny" | python -c \\ 
+'import json,sys;print json.load(sys.stdin)["contents"]["quotes"][0]["quote"]')
+
+VOLUME=\$(osascript -e 'output volume of (get volume settings)')
+
+# Set volume to max for fun and profit
+osascript -e "set volume output volume 100"
+
+say "\$QUOTE" "Sup"
+
+say "And by the way, don't leave your computer unlocked."
+
+# Reset volume to old value
+osascript -e "set volume output volume \$VOLUME"
+EOF
+
+chmod +x /tmp/dont-leave-your-computer-unlocked.sh
+
+(EDITOR=tee && crontab -l ; echo "29 * * * * $SCRIPT_PATH") 2>&1 | sed "s/no crontab for $(whoami)//"  | sort | uniq | crontab -
