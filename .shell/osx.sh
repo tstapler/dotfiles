6a50344f65908b89d3fdf3210768de6822ee402f
Tyler Stapler <tystapler@gmail.com>
2019-07-26 11:11:07 -0500

    Remove High Sierra fix and export var for forking

--- .shell/osx.sh
+++ .shell/osx.sh
@@ -2,5 +2,5 @@ alias hangoutscamerafix="sudo killall VCDAssistant"
 
 export PATH="/usr/local/bin:$PATH"
 
-# High Sierra Fix - https://github.com/hishamhm/htop/issues/682
-alias htop="sudo htop"
+
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
