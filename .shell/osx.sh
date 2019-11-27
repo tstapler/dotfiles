b944579762b2433b67363689a8efa5fe8f00155f
Tyler Stapler <tystapler@gmail.com>
2019-11-27 08:24:31 -0800

    Add python3.7 binaries to path

--- .shell/osx.sh
+++ .shell/osx.sh
@@ -2,5 +2,7 @@ alias hangoutscamerafix="sudo killall VCDAssistant"
 
 export PATH="/usr/local/bin:$PATH"
 
+export PATH="$PATH:/Users/tylerstapler/Library/Python/3.7/bin"
+
 
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
