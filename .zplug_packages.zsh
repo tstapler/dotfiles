ae57bd8b2af83c2dca4d3f3a96e6b11d8f314520
Tyler Stapler <tystapler@gmail.com>
2020-02-08 13:46:05 -0600

    Add ohmyzsh plugins using zplug

--- .zplug_packages.zsh
+++ .zplug_packages.zsh
@@ -11,11 +11,17 @@ zplug "bhilburn/powerlevel9k", as:theme
 zplug "zsh-users/zsh-completions"
 zplug "Tarrasch/zsh-autoenv"
 zplug "b4b4r07/enhancd", use:"init.sh"
+
+# ZSH completion from various sources
 zplug "zsh-users/zaw"
+
+# Command line completion engine
+zplug "clvv/fasd", as:command
 
 zplug "lib/completion", from:oh-my-zsh
 zplug "plugins/httpie", from:oh-my-zsh
 zplug "aswitalski/oh-my-zsh-sensei-git-plugin"
+zplug "plugins/git", from:oh-my-zsh
 zplug "djui/alias-tips"
 
 # The file searchers
@@ -23,6 +29,7 @@ zplug "djui/alias-tips"
 case $(uname) in
 	Darwin) 
 		BIN_ARCH=darwin
+		zplug "plugins/osx", from:oh-my-zsh
 		;;
 	*)
 		BIN_ARCH=linux
