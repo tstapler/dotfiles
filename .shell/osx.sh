98c10baae6cef9c5b3ef08e8a6a9f82c1665c64b
Tyler Stapler <tystapler@gmail.com>
2020-06-27 15:40:41 -0500

    Add homebrew path and remove hardcoded python 3 bin

--- .shell/osx.sh
+++ .shell/osx.sh
@@ -1,8 +1,7 @@
 alias hangoutscamerafix="sudo killall VCDAssistant"
 
+# Add homebrew sbin to path
+export PATH="/usr/local/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
-
-export PATH="$PATH:/Users/tylerstapler/Library/Python/3.7/bin"
-
 
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
