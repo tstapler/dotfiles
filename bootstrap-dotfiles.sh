748155661a3c8b72731197fb23fd13934a87ad0a
Tyler Stapler <tystapler@gmail.com>
2022-05-01 12:38:04 -0700

    feat: Update dotfiles bootstrap script to use ansible
    The script installs python using pyenv and then installs ansible to make it more portable

--- bootstrap-dotfiles.sh
+++ bootstrap-dotfiles.sh
@@ -3,62 +3,28 @@ REPO_NAME=dotfiles
 CLONE_DIR="$HOME/$REPO_NAME"
 DOTFILES_REPO="tstapler/$REPO_NAME"
 
-haveProg() {
-  [ -x "$(which "$1")" ]
-}
 
-install_package() {
-if haveProg apt-get ; then sudo apt-get update && sudo apt-get install -y "$@"
-elif haveProg yum ; then sudo yum install -y "$@"
-elif haveProg pacman ; then sudo pacman -S --noconfirm "$@"
-elif haveProg brew; then brew install "$@"
-else
-  echo 'Current package manager not supported!'
-  echo "Please install $@ manually"
-  exit 2
+# Install python
+if ! [ -d "$HOME/.pyenv/" ]; then
+  curl https://pyenv.run | bash
 fi
-}
 
-PYTHON_EXE=python
-PIP_PACKAGE_NAME=python-pip
-PIP_EXE_NAME=pip
-PIP_ARGS="--user"
-
-OS="$(uname -a)"
-
-case $OS in
-	*Debian*)
-		;;
-	*Ubuntu*)
-		;;
-  *\#1-Microsoft*)
-		;;
-	*MANJARO*|*ARCH*)
-		;;	
-	*Darwin*)
-	if ! haveProg brew; then
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		brew install python
-	fi
-	echo "Detected OSX"
-	PYTHON_EXE=python3
-	PIP_PACKAGE_NAME=python
-	PIP_EXE_NAME=pip3
-	BREW_PATH="export PATH="$PATH:$(brew --prefix)/lib/$(python3 --version | awk '{ print $2}')/site-packages
-	PROFILE="$HOME/.profile"
-	grep -qxF "$BREW_PATH" "$PROFILE" || sudo echo "$BREW_PATH" >> "$PROFILE"
-		;;
-  *)
-    echo "OS Not supported"
-esac 
-
-if ! haveProg git; then
-  install_package git
+PYTHON_VERSION=3.9.12
+export PYENV_VERSION=$PYTHON_VERSION
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+if ! [ -d "$HOME/.pyenv/versions/$PYTHON_VERSION/" ]; then
+  pyenv install 3.9.12
 fi
+
+pyenv exec python -m pip install pipx
+pyenv exec pipx ensurepath
+pyenv exec pipx install ansible
 
 if [ ! -d "$HOME/dotfiles" ]; then
   echo "Cloning dotfiles"
-  git clone "ssh://git@github.com/$DOTFILES_REPO" "$CLONE_DIR" || git clone "https://github.com/$DOTFILES_REPO" "$CLONE_DIR"
+  GIT_CONFIG_NOSYSTEM=1 git clone "https://github.com/$DOTFILES_REPO" "$CLONE_DIR"
 else
   echo "Skipping directory creation, $CLONE_DIR already exists."
 fi
@@ -66,28 +32,16 @@ fi
 echo "Checking out and updating submodules"
 cd "$CLONE_DIR" && git submodule update --init --recursive
 
-CFG_CADDY_DIR="$CLONE_DIR"/cfgcaddy
 
-if [ -d "$CFG_CADDY_DIR" ]; then
-  echo "Installing cfgcaddy dependencies."
+echo "Installing cfgcaddy dependencies."
 
-  if ! haveProg $PIP_EXE_NAME; then
-    install_package $PIP_PACKAGE_NAME
-  fi
+pyenv exec pipx install cfgcaddy
+pyenv exec cfgcaddy init "$CLONE_DIR" "$HOME"
 
-  $PIP_EXE_NAME install $PIP_ARGS --editable "$CFG_CADDY_DIR"
+echo "Linking Dotfiles"
+pyenv exec cfgcaddy link
 
-  $PYTHON_EXE -m cfgcaddy init "$CLONE_DIR" "$HOME"
+cd "$HOME/dotfiles/stapler-scripts/" || exit
 
-  echo "Linking Dotfiles"
-  $PYTHON_EXE -m cfgcaddy link
-else 
-  echo "cfgcaddy repo is not present, cannot link dotfiles"
-fi
-
-for dep in zsh gpg keychain; do
-  if ! hash $dep 2>/dev/null; then
-    echo "Installing missing dependency $dep"
-    install_package $dep
-  fi
-done
+pyenv exec ansible-galaxy install -r requirements.yml
+pyenv exec ansible-playbook -vvv -K bootstrap.yaml
