286cdd94f5a634ad792b8990b47bf8bf901fbc91
Tyler Stapler <tystapler@gmail.com>
2022-05-18 16:34:58 -0700

    feat(asdf): Add support for development headers with ASDF python versions

--- .shell/languages.sh
+++ .shell/languages.sh
@@ -10,6 +10,9 @@ if [ -d "$ASDF_DIR" ]; then
   . "$ASDF_DIR/asdf.sh"
 fi
 
+# This will cause the dev headers to work for python when it is built by asdf
+export PYTHON_CONFIGURE_OPTS="--enable-shared"
+
 if hash asdf 2>/dev/null; then
 ASDF_PLUGIN_DIR="$ASDF_DIR/plugins"
 # Add ASDF plugins if they don't exist
