453a83efbd10ae8f224d23743aa36f67c2147266
Tyler Stapler <tystapler@gmail.com>
2023-09-10 08:26:52 -0700

    feat: Update the unlocked script to maybe work on linux as well

--- dont-leave-things-unlocked.sh
+++ dont-leave-things-unlocked.sh
@@ -1,34 +1,43 @@
 export SCRIPT_PATH=/tmp/dont-leave-your-computer-unlocked.sh
 
-# In order to run this script use the command:
-# Make sure to include the leading space so it doesn't get logged to history
-# source <(curl -L unlocked.staplerstation.com)
-
 cat << EOF > $SCRIPT_PATH
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
+# Sleep for a random duration
 sleep \$((RANDOM % 400 ))
 
-# Get a funny quote to say
-QUOTE=\$(curl "http://quotes.rest/qod.json?category=funny" | python -c 'import json,sys;print(json.load(sys.stdin)["contents"]["quotes"][0]["quote"])')
+# Fetch a funny quote
+QUOTE=\$(curl -s "http://quotes.rest/qod.json?category=funny" | python3 -c 'import json,sys;quote=json.load(sys.stdin)["contents"]["quotes"][0]["quote"];print(quote)')
 
-VOLUME=\$(osascript -e 'output volume of (get volume settings)')
+# Get OS
+OS=\$(uname)
 
-# Set volume to max for fun and profit
-osascript -e "set volume output volume 100"
+# MacOS specific
+if [[ "\$OS" == "Darwin" ]]; then
+    VOLUME=\$(osascript -e 'output volume of (get volume settings)')
+    osascript -e "set volume output volume 100"
+    say "\$QUOTE"
+    say "And by the way, don't leave your computer unlocked."
+    osascript -e "set volume output volume \$VOLUME"
 
-# Give it to em
-say "\$QUOTE"
-say "And by the way, don't leave your computer unlocked."
-
-# Reset volume to old value
-osascript -e "set volume output volume \$VOLUME"
+# Linux specific (using amixer for volume and spd-say for speech)
+elif [[ "\$OS" == "Linux" ]]; then
+    VOLUME=\$(amixer get Master | grep -oP '\[\d+%\]' | head -1 | tr -d '[]%')
+    amixer set Master 100%
+    spd-say "\$QUOTE"
+    spd-say "And by the way, don't leave your computer unlocked."
+    amixer set Master \$VOLUME%
+fi
 EOF
 
 chmod +x /tmp/dont-leave-your-computer-unlocked.sh
 
-(EDITOR=tee && crontab -l ; echo "29 * * * * $SCRIPT_PATH") 2>&1 | sed "s/crontab: no crontab for $(whoami)//"  | sort | uniq | crontab -
+# Append to crontab if not present
+if ! crontab -l | grep -q "$SCRIPT_PATH"; then
+    (EDITOR=tee && crontab -l ; echo "29 * * * * $SCRIPT_PATH") 2>&1 | sed "s/crontab: no crontab for $(whoami)//"  | sort | uniq | crontab -
+fi
 
-# To disable the script once your fun is done:
-# touch empty && crontab empty
-# rm /tmp/dont*.sh
+# Inform the user about the prank
+echo "Prank set. To disable:"
+echo "touch empty && crontab empty"
+echo "rm /tmp/dont*.sh"
